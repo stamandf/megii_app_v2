592eceb1e274cc34d308b13489ef19a6c7c23842
Florence <stamandf@gmail.com>
2022-07-16 15:59:41 -0400

    Change axios baseUrl.

--- src/services/actions/listings.js
+++ src/services/actions/listings.js
@@ -1,5 +1,6 @@
 import axios from 'axios';
-const baseUrl = 'http://localhost:3001/api/listings';
+const baseUrl = '/api/listings';
+// const baseUrl = 'http://localhost:3001/api/listings';
 
 const getAll = () => {
     console.log('getALL!');
